9666fb5072cff6a22cc76359d734c4c933b37470
Chris Benesch <chris@beneschtech.com>
2019-09-04 01:43:31 +0000

    Update to GPL license

--- src/until.c
+++ src/until.c
@@ -1,26 +1,20 @@
-/*
- * MIT License
- *
- * Copyright (c) 2019 Chris Benesch (chris@beneschtech.com)
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
+/**
+ *     until - run commands with a timeout
+ *     Copyright (C) 2019 Chris Benesch <chris@beneschtech.com>
+ * 
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -129,7 +123,11 @@ bool parse_args(int argc, char **argv)
 void usage()
 {
     const char *usageStr =
-            PACKAGE_STRING "\n\n"
+            PACKAGE_STRING "Copyright (C) 2019 Chris Benesch <chris@beneschtech.com>\n\n"
+            "This program comes with ABSOLUTELY NO WARRANTY;\n" 
+            "This is free software, and you are welcome to redistribute it\n"
+            "under certain conditions; see https://www.gnu.org/licenses for details.\n"
+            "\n"
             "A utility that lets you run a command with a timeout to eliminate hung processes\n"
             "\n"
             "Usage: until (timeout) [options] command args...\n"
